53cb97200f601b34f31a25769576853d974a9599
Sudarshan Mehatre <sudarshan.mehatre@salesforce.com>
2023-10-04 23:09:01 +0530

    optimize leetcode #1272
    Signed-off-by: Sudarshan Mehatre <sudarshan.mehatre@salesforce.com>

--- premium-algo-100/intervals/lc1272.cpp
+++ premium-algo-100/intervals/lc1272.cpp
@@ -1,3 +1,35 @@
+//problem: https://leetcode.com/problems/remove-interval/
+
+class Solution {
+public:
+    vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& r) {
+        
+        vector<vector<int>> res;
+        int n=intervals.size();
+
+        int start = r[0], end = r[1];
+        for(auto i:intervals)
+        {
+            int a = i[0];
+            int b = i[1];
+            
+            if(a>end || b<start)
+                res.push_back({a,b});
+            else
+            {
+                if(a<start)
+                    res.push_back({a,start});
+                
+                if(b>end)
+                    res.push_back({end,b});
+            }
+        }
+
+        return res;
+    }
+};
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class Solution {
 public:
     vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& r) {
